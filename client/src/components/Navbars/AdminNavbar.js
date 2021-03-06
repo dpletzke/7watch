4dbb67fa41c3de048e0f4012188416ec17393562
daniel <dpletzke@gmail.com>
2021-03-06 03:39:58 -0600

    gotta be const

--- client/src/components/Navbars/AdminNavbar.js
+++ client/src/components/Navbars/AdminNavbar.js
@@ -15,7 +15,7 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React, { Component } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import { Navbar, Container, Nav, Dropdown, Button } from "react-bootstrap";
 
@@ -26,7 +26,7 @@ function Header() {
   const mobileSidebarToggle = (e) => {
     e.preventDefault();
     document.documentElement.classList.toggle("nav-open");
-    var node = document.createElement("div");
+    const node = document.createElement("div");
     node.id = "bodyClick";
     node.onclick = function () {
       this.parentElement.removeChild(this);
