e67afbf5ce392a678c3512f8596614315e325f83
daniel <dpletzke@gmail.com>
2021-03-07 03:34:03 -0600

    setup config to set from config object

--- client/src/config.js
+++ client/src/config.js
@@ -6,13 +6,17 @@ const config = {
   SERVER_HTTP_HOST: "localhost",
 };
 
-const specifyConfig = (input) => {
-  for (let key of Object.values(input)) {
-    if      (key === "exHl7Port") config.HL7_EXTERNAL_PORT = input.exHl7Port;
-    else if (key === "inHl7Port") config.HL7_INTERNAL_PORT = input.inHl7Port;
-    else if (key === "clHttpPort") config.CLIENT_HTTP_PORT = input.clHttpPort;
-    else if (key === "svHttpPort") config.SERVER_HTTP_PORT = input.svHttpPort;
-    else if (key === "svHttpHost") config.SERVER_HTTP_HOST = input.svHttpHost;
+const specifyConfig = ({ formConfig }) => {
+  for (let key of Object.keys(formConfig)) {
+    if (key === "exHl7Port") config.HL7_EXTERNAL_PORT = formConfig.exHl7Port;
+    else if (key === "inHl7Port")
+      config.HL7_INTERNAL_PORT = formConfig.inHl7Port;
+    else if (key === "clHttpPort")
+      config.CLIENT_HTTP_PORT = formConfig.clHttpPort;
+    else if (key === "svHttpPort")
+      config.SERVER_HTTP_PORT = formConfig.svHttpPort;
+    else if (key === "svHttpHost")
+      config.SERVER_HTTP_HOST = formConfig.svHttpHost;
     else throw new Error(`Invalid config key ${key}`);
   }
 };
