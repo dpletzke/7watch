ff273c66adccfb0345520aac4ed092694cf1dc6d
daniel <dpletzke@gmail.com>
2021-03-08 02:40:17 -0600

    add dynamic adding of logs to table on message recieve

--- client/src/views/Logs.jsx
+++ client/src/views/Logs.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 // react-bootstrap components
 import Badge from "react-bootstrap/Badge";
@@ -11,12 +11,43 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+import { useLogsStore } from "../contexts/logsContext";
+
+import { normalizeMsg } from "../helpers/msgProcessing";
+
 const { ipcRenderer } = window.require("electron");
 
 function Logs() {
+  const logsStore = useLogsStore();
 
+  useEffect(() => {
+    ipcRenderer.on("msg_recieved", (e, msg) => {
+      console.log(msg);
 
+      logsStore.addLog(msg);
+    });
+  }, [logsStore]);
 
+  const logRow = (data) => {
+    const msg = normalizeMsg(data);
+
+    const obr = msg.segments.find((sg) => sg.name === "OBR");
+    const obxs = msg.segments.filter((sg) => sg.name === "OBX");
+    console.log({ data, obr, obxs });
+    return (
+      <tr key={data.id}>
+        <td>{msg.header.fields.sentTimeStamp}</td>
+        <td>{obr.fields.observationTimeStamp}</td>
+        <td>{obr.fields.collectorIdentifier}</td>
+        <td>
+          {obxs.map(({ fields }) => {
+            const { observationIdentifier, value } = fields;
+            return <p>{`${observationIdentifier}, ${value}`}</p>;
+          })}
+        </td>
+      </tr>
+    );
+  };
 
   return (
     <>
@@ -26,65 +57,19 @@ function Logs() {
             <Card className="card-plain table-plain-bg">
               <Card.Header>
                 <Card.Title as="h4">All Logs</Card.Title>
-                <p className="card-category">
-                  Last 200 messages
-                </p>
+                <p className="card-category">Last 200 messages</p>
               </Card.Header>
               <Card.Body className="table-full-width table-responsive px-0">
                 <Table className="table-hover">
                   <thead>
                     <tr>
-                      <th className="border-0">Index</th>
-                      <th className="border-0">TimeStamp</th>
+                      <th className="border-0">Time Sent</th>
+                      <th className="border-0">Time Collected</th>
                       <th className="border-0">Device Id</th>
                       <th className="border-0">Variables</th>
-                      <th className="border-0">Values</th>
                     </tr>
                   </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Dakota Rice</td>
-                      <td>$36,738</td>
-                      <td>Niger</td>
-                      <td>Oud-Turnhout</td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>Minerva Hooper</td>
-                      <td>$23,789</td>
-                      <td>Curaçao</td>
-                      <td>Sinaai-Waas</td>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                      <td>Sage Rodriguez</td>
-                      <td>$56,142</td>
-                      <td>Netherlands</td>
-                      <td>Baileux</td>
-                    </tr>
-                    <tr>
-                      <td>4</td>
-                      <td>Philip Chaney</td>
-                      <td>$38,735</td>
-                      <td>Korea, South</td>
-                      <td>Overland Park</td>
-                    </tr>
-                    <tr>
-                      <td>5</td>
-                      <td>Doris Greene</td>
-                      <td>$63,542</td>
-                      <td>Malawi</td>
-                      <td>Feldkirchen in Kärnten</td>
-                    </tr>
-                    <tr>
-                      <td>6</td>
-                      <td>Mason Porter</td>
-                      <td>$78,615</td>
-                      <td>Chile</td>
-                      <td>Gloucester</td>
-                    </tr>
-                  </tbody>
+                  <tbody>{logsStore.logs.map((log) => logRow(log))}</tbody>
                 </Table>
               </Card.Body>
             </Card>
