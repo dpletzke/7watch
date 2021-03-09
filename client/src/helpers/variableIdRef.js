e8e7066d9d1ff9c2941c24cf484b36fb92dd4d25
daniel <dpletzke@gmail.com>
2021-03-09 03:28:09 -0600

    create initial reference for devices and variables

---
+++ client/src/helpers/variableIdRef.js
@@ -0,0 +1,291 @@
+export const varRef = new Map([
+  [
+    2,
+    {
+      id: 2,
+      common: "NIBP (systolic)",
+      full: "NON INV BLOOD PRESS SYS (NIBP)",
+    },
+  ],
+  [
+    3,
+    {
+      id: 3,
+      common: "NIBP (diastolic)",
+      full: "NON INV BLOOD PRESS DIAS (NIBP)",
+    },
+  ],
+  [
+    4,
+    {
+      id: 4,
+      common: "NIBP Blood Pressure Mean",
+      full: "NON INV BLOOD PRESSURE MEAN (NIBP)",
+    },
+  ],
+  [1, { id: 1, common: "HEART RATE", full: "HEART RATE (PULSE RATE)" }],
+  [14, { id: 14, common: "SPO2", full: "PULSE OXIMETRY (SPO2)" }],
+  [22, { id: 22, common: "RESPIRATORY RATE", full: "RESPIRATION RATE" }],
+  [
+    519,
+    {
+      id: 519,
+      common: "CVP",
+      full: "INV BLOOD PRESS MEAN CENTRAL VENOUS (CVP)",
+    },
+  ],
+  [
+    20,
+    {
+      id: 20,
+      common: "ETCO2",
+      full: "END TIDAL CO2 (ETCO2)",
+    },
+  ],
+  [
+    501,
+    {
+      id: 501,
+      common: "INVASIVE BLOOD PRESSURE Systolic",
+      full: "INV BLOOD PRESS SYS ARTERIAL (ABP)",
+    },
+  ],
+  [
+    502,
+    {
+      id: 502,
+      common: "INVASIVE BLOOD PRESSURE Diastolic",
+      full: "INV BLOOD PRESS DIAS ARTERIAL (ABP)",
+    },
+  ],
+  [
+    503,
+    {
+      id: 503,
+      common: "INVASIVE BLOOD PRESSURE Mean",
+      full: "INV BLOOD PRESS MEAN ARTERIAL (MAP)",
+    },
+  ],
+  [
+    504,
+    {
+      id: 504,
+      common: "PULMONARY ARTERY PRESSURE SYSTOLIC",
+      full: "INV BLOOD PRESS SYS PULMONARY ARTERY (PAP)",
+    },
+  ],
+  [
+    505,
+    {
+      id: 505,
+      common: "PULMONARY ARTERY PRESSURE DIASTOLIC",
+      full: "INV BLOOD PRESS DIAS PULMONARY ARTERY (PAP)",
+    },
+  ],
+  [
+    509,
+    {
+      id: 509,
+      common: "PCWP",
+      full: "INV BLOOD PRESS PULMONARY WEDGE (PCWP)",
+    },
+  ],
+  [63, { id: 63, common: "CARDIAC OUTPUT", full: "CARDIAC OUTPUT" }],
+  [73, { id: 73, common: "SvO2", full: "MIXED VENOUS O2 SATURATION (SvO2)" }],
+  [2409, { id: 2409, common: "BISPECTRAL INDEX", full: "BISPECTRAL INDEX" }],
+  [
+    605,
+    {
+      id: 605,
+      common: "AGENT ISOFLURANE INSPIRED",
+      full: "AGENT ISOFLURANE INSPIRED",
+    },
+  ],
+  [
+    606,
+    {
+      id: 606,
+      common: "AGENT ISOFLURANE EXPIRED",
+      full: "AGENT ISOFLURANE EXPIRED",
+    },
+  ],
+  [
+    595,
+    {
+      id: 595,
+      common: "AGENT SEVOFLURANE INSPIRED",
+      full: "AGENT SEVOFLURANE INSPIRED",
+    },
+  ],
+  [
+    596,
+    {
+      id: 596,
+      common: "AGENT SEVOFLURANE EXPIRED",
+      full: "AGENT SEVOFLURANE EXPIRED",
+    },
+  ],
+  [
+    3573,
+    {
+      id: 3573,
+      common: "TEMPERATURE ESOPHOGEAL",
+      full: "TEMPERATURE ESOPHOGEAL",
+    },
+  ],
+  [
+    1189,
+    {
+      id: 1189,
+      common: "PEEP",
+      full: "POSITIVE END EXPIRATORY PRESS (PEEP)",
+    },
+  ],
+  [
+    584,
+    { id: 584, common: "OPERATION MODE (VENT)", full: "OPERATION MODE (VENT)" },
+  ],
+  [3570, { id: 3570, common: "TEMPERATURE SKIN", full: "TEMPERATURE SKIN" }],
+  [
+    3572,
+    {
+      id: 3572,
+      common: "TEMPERATURE NASOPHARYNGEAL",
+      full: "TEMPERATURE NASOPHARYNGEAL",
+    },
+  ],
+  [
+    2583,
+    {
+      id: 2583,
+      common: "(I/E) (VENT)",
+      full: "INSPIRATION/EXPIRATION RATIO (I/E) (VENT)",
+    },
+  ],
+  [
+    4024,
+    { id: 4024, common: "AGENT FLOW RATE O2", full: "AGENT FLOW RATE O2" },
+  ],
+  [
+    4115,
+    { id: 4115, common: "AGENT FLOW RATE N2O", full: "AGENT FLOW RATE N2O" },
+  ],
+  [
+    4113,
+    { id: 4113, common: "AGENT FLOW RATE AIR", full: "AGENT FLOW RATE AIR" },
+  ],
+  [
+    635,
+    {
+      id: 635,
+      common: "FIO2",
+      full: "O2 INSPIRED FRACTION (FIO2)",
+    },
+  ],
+  [
+    60,
+    {
+      id: 60,
+      common: "TIDAL VOLUME OBSERVED (VENT)",
+      full: "TIDAL VOLUME OBSERVED (VENT)",
+    },
+  ],
+  [
+    2564,
+    {
+      id: 2564,
+      common: "PIP (VENT)",
+      full: "PEAK INSPIRATORY PRESS (PIP) (VENT)",
+    },
+  ],
+  [
+    608,
+    {
+      id: 608,
+      common: "ETCO2 (VENT)",
+      full: "CO2 END-TIDAL (ETCO2) (VENT)",
+    },
+  ],
+  [
+    3754,
+    {
+      id: 3754,
+      common: "TEMPERATURE GENERAL (CORE BODY)",
+      full: "TEMPERATURE GENERAL (CORE BODY)",
+    },
+  ],
+  [
+    19,
+    {
+      id: 19,
+      common: "Anesthesia EKG Heart rate",
+      full: "AN HEART RATE (EKG)",
+    },
+  ],
+  [
+    4436,
+    {
+      id: 4436,
+      common: "IABP ASSISTED SYSTOLIC PRESSURE ",
+      full: "IABP ASSISTED SYSTOLIC PRESSURE",
+    },
+  ],
+  [
+    4438,
+    {
+      id: 4438,
+      common: "IABP ASSISTED END DIASTOLIC PRESSURE ",
+      full: "IABP ASSISTED END DIASTOLIC PRESSURE ",
+    },
+  ],
+  [
+    4432,
+    {
+      id: 4432,
+      common: "IABP UNASSISTED SYSTOLIC (US)",
+      full: "IABP UNASSISTED SYSTOLIC (US)",
+    },
+  ],
+  [
+    4434,
+    {
+      id: 4434,
+      common: "IABP UEDP",
+      full: "IABP UNASSISTED END DIASTOLIC PRESSURE (UEDP)",
+    },
+  ],
+  [
+    4338,
+    {
+      id: 4338,
+      common: "IABP DIASTOLIC AUGMENTED (DA)",
+      full: "IABP DIASTOLIC AUGMENTED (DA)",
+    },
+  ],
+  [10, { id: 10, common: "IABP MEAN ", full: "IABP MEAN " }],
+  [4342, { id: 4342, common: "IABP MODE", full: "IABP MODE" }],
+  [
+    513,
+    {
+      id: 513,
+      common: null,
+      full: "PRS - Generic Pressure Being used as Femoral Art",
+    },
+  ],
+  [
+    514,
+    {
+      id: 514,
+      common: null,
+      full: "PRS - Generic Pressure Being used as Femoral Art",
+    },
+  ],
+  [
+    515,
+    {
+      id: 515,
+      common: null,
+      full: "PRS - Generic Pressure Being used as Femoral Art",
+    },
+  ],
+]);
