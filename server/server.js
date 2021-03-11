7e8802bc7262862ec02fa2f55973fbdb96b6bbb6
daniel <dpletzke@gmail.com>
2021-03-10 23:57:42 -0600

    thinking on nedb arch

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
+const hl7 = require("simple-hl7");
 
-const hl7 = require('simple-hl7');
-
-const { HL7_INTERNAL_PORT, HL7_EXTERNAL_PORT } = require('./constants');
+const { HL7_INTERNAL_PORT, HL7_EXTERNAL_PORT } = require("./constants");
 
 const server = hl7.tcp();
 
+// datastores options
+// for
+// by minute
+// by hour
+//
+
 const client = hl7.Server.createTcpClient({
-  host: 'localhost',
+  host: "localhost",
   port: HL7_INTERNAL_PORT,
   keepalive: true,
   callback: (err, ack) => {
@@ -16,30 +21,29 @@ const client = hl7.Server.createTcpClient({
     } else {
       console.log(ack.log());
     }
-  }
+  },
 });
 
 server.use((req, res, next) => {
   //req.msg is the HL7 message
-  console.log('******message received*****')
+  console.log("******message received*****");
   console.log(req.msg.log());
 
-  console.log('******message forwarded*****')
+  console.log("******message forwarded*****");
   client.send(req.msg);
   next();
-})
+});
 
 server.use((err, req, res, next) => {
   //error handler
   //standard error middleware would be
-  console.log('******ERROR*****')
+  console.log("******ERROR*****");
   console.log(err);
-  const msa = res.ack.getSegment('MSA');
-  msa.setField(1, 'AR');
-  res.ack.addSegment('ERR', err.message);
+  const msa = res.ack.getSegment("MSA");
+  msa.setField(1, "AR");
+  res.ack.addSegment("ERR", err.message);
   res.end();
 });
 
 //Listen on port 7777
 server.start(HL7_EXTERNAL_PORT);
-
