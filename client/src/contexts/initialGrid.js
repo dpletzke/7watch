097b80cfc421d672f641d08a77b590f6b667a23c
daniel <dpletzke@gmail.com>
2021-03-14 03:50:07 -0500

    fixtures for testing

---
+++ client/src/contexts/initialGrid.js
@@ -0,0 +1,17 @@
+// prettier-ignore
+export const initialObservations = [
+  {id:2, common:'NIBP (systolic)', full:'NON INV BLOOD PRESS SYS (NIBP)'},
+  {id:3, common:'NIBP (diastolic)', full:'NON INV BLOOD PRESS DIAS (NIBP)'},
+  {id:73, common:'SvO2', full:'MIXED VENOUS O2 SATURATION (SvO2)'},
+  {id:519, common:'CVP', full:'INV BLOOD PRESS MEAN CENTRAL VENOUS (CVP)'},
+  {id:635, common:'FIO2', full:'O2 INSPIRED FRACTION (FIO2)'},
+]
+
+export const initialDeviceIds = [
+  "CARDBAY1",
+  "CARDBAY2",
+  "CARDECHO",
+  "CARDSTRESSRM2",
+  "CARDSTRESSRM3",
+  "CARDSTRESSRM5",
+];
