95b917070a6de852fa4d08b7471cfdc8df6dbded
daniel <dpletzke@gmail.com>
2021-03-14 16:03:55 -0500

    switch row and column on display bug

--- client/src/components/Grid/Square.jsx
+++ client/src/components/Grid/Square.jsx
@@ -8,22 +8,23 @@ const Square = observer((props) => {
   const gridStore = useGridStore();
 
   const [deviceId, observationId] = useMemo(() => {
-    return gridStore.getIds(columnIndex, rowIndex);
+    // offset row and column index for header columns
+    const ids = gridStore.getIds(rowIndex - 1, columnIndex - 1);
+    return ids;
   }, [columnIndex, gridStore, rowIndex]);
 
   let value;
-
-  if (columnIndex === 0) {
+  if (columnIndex === 0 && rowIndex === 0) {
+    value = "Table";
+  } else if (columnIndex === 0 && deviceId) {
     value = deviceId;
-  } else if (rowIndex === 0) {
+  } else if (rowIndex === 0 && gridStore.observations.get(observationId)) {
     value = gridStore.observations.get(observationId).common;
-  } else {
+  } else if (deviceId && gridStore.observations.get(observationId)) {
     value = gridStore.getValue(deviceId, observationId);
   }
 
-  return (
-      <div>{value}</div>
-  );
-})
+  return <div>{value}</div>;
+});
 
 export default Square;
