3fa11cd11d77c0e9d77d45e6a22fb63c0c95bb6c
daniel <dpletzke@gmail.com>
2021-03-17 02:37:28 -0500

    use mobx runinaction, due to: async function changing store

--- client/src/contexts/gateContext.js
+++ client/src/contexts/gateContext.js
@@ -1,28 +1,36 @@
 import React from "react";
 import { useLocalObservable } from "mobx-react-lite";
+import { runInAction } from "mobx";
 import { config } from "../config";
 const { ipcRenderer } = window.require("electron");
 
 const GateContext = React.createContext(null);
 
-export const GateProvider = ({ children }) => {
-  const gateStore = useLocalObservable(() => {
-    return {
-      isOpen: false,
-      open: async function () {
-        if (!this.isOpen) {
-          await ipcRenderer.invoke("start_gate", config);
+// exported for testing only
+export const createGateStore = () => {
+  return {
+    isOpen: false,
+    open: async function () {
+      if (!this.isOpen) {
+        await ipcRenderer.invoke("start_gate", config);
+        runInAction(() => {
           this.isOpen = true;
-        }
-      },
-      close: async function () {
-        if (this.isOpen) {
-          await ipcRenderer.invoke("stop_gate");
+        });
+      }
+    },
+    close: async function () {
+      if (this.isOpen) {
+        await ipcRenderer.invoke("stop_gate");
+        runInAction(() => {
           this.isOpen = false;
-        }
-      },
-    };
-  });
+        });
+      }
+    },
+  };
+};
+
+export const GateProvider = ({ children }) => {
+  const gateStore = useLocalObservable(createGateStore);
 
   return (
     <GateContext.Provider value={gateStore}>{children}</GateContext.Provider>
