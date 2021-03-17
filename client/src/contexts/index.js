930273e76f46e259baf4d8883fc18ba3f79c33f9
daniel <dpletzke@gmail.com>
2021-03-17 03:16:28 -0500

    add strict mobx linting

--- client/src/contexts/index.js
+++ client/src/contexts/index.js
@@ -2,6 +2,16 @@ import { LogsProvider, useLogsStore } from "./logsContext";
 import { GateProvider, useGateStore } from "./gateContext";
 import { GridProvider, useGridStore } from "./gridContext";
 
+import { configure } from "mobx";
+
+configure({
+  enforceActions: "always",
+  computedRequiresReaction: true,
+  reactionRequiresObservable: true,
+  observableRequiresReaction: true,
+  disableErrorBoundaries: true,
+});
+
 export default function IndexProvider({ children }) {
   return (
     <GridProvider>
