abc1ab96d39dffe3d8d21243a4fa00279b73ac85
daniel <dpletzke@gmail.com>
2021-03-20 19:53:48 -0500

    defer close to after save state, assure close with timer

--- client/main/ipcRoutes.js
+++ client/main/ipcRoutes.js
@@ -1,19 +1,44 @@
-const { ipcMain } = require("electron");
+const { app, ipcMain } = require("electron");
 const { replaceDatabase, retrievePreviousState } = require("./db/db.js");
 
-module.exports = (win, gateControls) => {
+module.exports = (window, gateControls) => {
+  let win = window;
   const { startGate, stopGate } = gateControls;
   // TODO implement in renderer listen previous state
-  win.webContents.send("previous_state", retrievePreviousState());
-
+  retrievePreviousState()
+    .then((appState) => {
+      win.webContents.send("previous_state", appState);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  /**
+   * since we redirect the close event, set a timer to close the app
+   * if the renderer doesn't respond quick enough
+   * consequence: changes in state made during that session won't be saved
+   */
+  let closeAssuranceTimer;
   win.on("close", (e) => {
-    e.preventDefault();
-    // TODO implement in renderer send save_app_state
-    win.webContents.send("closing_app");
+    if (win) {
+      e.preventDefault();
+      // TODO implement in renderer send save_app_state
+      closeAssuranceTimer = setTimeout(() => {
+        win = null;
+        app.quit();
+        clearTimeout(closeAssuranceTimer);
+      }, 3000);
+      win.webContents.send("closing_app");
+    }
   });
 
   ipcMain.handle("save_before_closing", async (event, appState) => {
+    clearTimeout(closeAssuranceTimer);
     await replaceDatabase(appState);
+    win = null;
+    // TODO do we need this if there is already a events listener for all windows closed?
+    // if (process.platform !== "darwin") {
+    //   app.quit();
+    // }
   });
 
   ipcMain.handle("start_gate", (event, config) => {
