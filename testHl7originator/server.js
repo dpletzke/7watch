fad02619abdb9efdf7e75e0fd9cfe2da766482af
daniel <dpletzke@gmail.com>
2021-03-29 01:17:20 -0500

    set up client

--- testHl7originator/server.js
+++ testHl7originator/server.js
@@ -1,46 +1,100 @@
-// const hl7 = require("../lib");
+const hl7 = require("simple-hl7");
+/**
+ * TODO test if parseing before sending is necessary
+ * @see https://github.com/hitgeek/simple-hl7/blob/master/examples/message.js
+ */
+// const parser = new hl7.Parser();
 
-// const parser = new hl7.Parser({ segmentSeperator: "\n" });
-// const client = hl7.Server.createFileClient("test-client");
-
+// fixtures
 const messages = [
-  { device: "APPLE", obxs: [101, 103] },
-  { device: "BANANA", obxs: [102, 104] },
-  { device: "RACCOON", obxs: [101, 103] },
-  { device: "ELEPHANT", obxs: [102, 104] },
+  { device: "CARDECHO", obxs: [101, 103] },
+  { device: "ENDOMONITOR130069", obxs: [102, 104] },
+  { device: "ANMONITOR35901", obxs: [101, 103] },
+  { device: "ICCURM7108", obxs: [102, 104] },
 ];
 
-function L(val) {
-  this.val = val;
-  this.next = null;
-}
+const client = hl7.Server.createTcpClient({
+  host: 'localhost',
+  port: 7777,
+  keepalive: true,
+  callback: function(err, ack) {
+    if (err) {
+      console.log(err.message);
+  }
+});
 
-function createL(arr) {
-  let node, temp, head;
+/**
+ * @param {Object} msg - fixtures containing msg details
+ * @param {string} msg.device - device Id
+ * @param {number[]} msg.obxs - array of observation ids, or OBX-3s to send 
+ * @returns message that can be sent with client.send(hl7Msg)
+ */
+const createHl7Message = (msg) => {
+  const { device, obxs } = msg;
+  const now = Date.now();
+  const hl7Msg = new hl7.Message("node-test-client", "", "", `${now}`, "", [
+    "ORU",
+    "R01",
+  ]);
+
+  hl7Msg.addSegment("OBR");
+  hl7Msg.getSegment("OBR").setField(7, now - 10000);
+  hl7Msg.getSegment("OBR").setField(10, device);
+  obxs.forEach((obx, i) => {
+    hl7Msg.addSegment(
+      "OBX",
+      `${i}`,
+      "NM",
+      `${obx}`,
+      "",
+      // random observation value that is similiar to obx for testing
+      `${obx * 10 + Math.random() * 18 - 9}`
+    );
+  });
+  return hl7Msg;
+};
+
+/**
+ * converts array into singley cyclical linked list
+ * @param {any[]} arr - array with any values
+ * @returns
+ */
+const createLL = (arr) => {
+  let node, temp, tail;
+  const listItem = (val) => {
+    return {
+      val,
+    next: null
+  }
+  };
   for (let i = arr.length - 1; i >= 0; i--) {
     if (!node) {
-      node = new L(arr[i]);
-      head = node;
+      node = new ListItem(arr[i]);
+      tail = node;
     } else {
-      temp = new L(arr[i]);
-      temp.next = i !== 0 ? node : head;
+      temp = new ListItem(arr[i]);
+      temp.next = node;
+      if (i === 0) {
+        tail.next = temp;
+      }
       node = temp;
     }
   }
   return node;
 }
 
-// const sendNextMessage = (file) => {
-//   if(!file) return;
-//   const msg = parser.parseFileSync(file);
-//   client.send(msg, (err) => {
-//     if (err) {
-//       console.error("ERR: " + err.message);
-//     }
-//   });
-//   const timer = setTimeout(() => {
-//     if()
-//   }, 1000);
-// };
+const sendNextMessageRecursive = (msg) => {
+  const hl7Msg = createHl7Message(msg);
 
-console.log(JSON.stringify(createL(messages)));
+  const timer = setTimeout(() => {
+    clearTimeout(timer);
+    sendNextMessageRecursive(msg.next);
+  }, 1000);
+};
+
+const startMessageCycle = (messages) => {
+  const headMessage = createLL(messages);
+  sendNextMessageRecursive(headMessage);
+};
+
+startMessageCycle(messages);
