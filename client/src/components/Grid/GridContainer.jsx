2912041c8bed2ec6b95efdbdd58e0a348d3c7c4c
daniel <dpletzke@gmail.com>
2021-04-20 01:13:51 -0500

    for testing load inital state on each render, create seperate components for each multi-grid section, change width and height based on index

--- client/src/components/Grid/GridContainer.jsx
+++ client/src/components/Grid/GridContainer.jsx
@@ -1,12 +1,20 @@
 /** @flow */
 // import Immutable from "immutable";
 // import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect } from "react";
 import { AutoSizer, MultiGrid } from "react-virtualized";
 import { observer } from "mobx-react-lite";
 
 import styles from "../../assets/css/MultiGrid.css";
+
 import Square from "./Square";
+import RowHeader from "./RowHeader";
+import ColumnHeader from "./ColumnHeader";
+
+import {
+  initialDeviceIds,
+  initialObservations,
+} from "../../contexts/initialGrid";
 import { useGridStore } from "../../contexts";
 
 const STYLE = {
@@ -29,10 +37,29 @@ const STYLE_TOP_RIGHT_GRID = {
 const GridContainer = observer(() => {
   const gridStore = useGridStore();
 
+  // for testing
+  useEffect(() => {
+    gridStore.destroyAll();
+    gridStore.addDevices(initialDeviceIds);
+    gridStore.addObservations(initialObservations);
+  });
+
   const _cellRenderer = ({ columnIndex, key, rowIndex, style }) => {
+    const indices = { columnIndex, rowIndex };
+    let cellContent;
+
+    if (columnIndex === 0 && rowIndex === 0) {
+      cellContent = "Table";
+    } else if (columnIndex === 0) {
+      cellContent = <RowHeader {...indices} />;
+    } else if (rowIndex === 0) {
+      cellContent = <ColumnHeader {...indices} />;
+    } else {
+      cellContent = <Square {...indices} />;
+    }
     return (
       <div className={styles.Cell} key={key} style={style}>
-        <Square {...{ columnIndex, rowIndex }} />
+        {cellContent}
       </div>
     );
   };
@@ -46,12 +73,12 @@ const GridContainer = observer(() => {
           scrollToColumn={0}
           scrollToRow={0}
           cellRenderer={_cellRenderer}
-          columnWidth={150}
+          columnWidth={({ index }) => (index === 0 ? 150 : 50)}
           columnCount={gridStore.observations.size + 1}
           enableFixedColumnScroll
           enableFixedRowScroll
           height={height}
-          rowHeight={20}
+          rowHeight={({ index }) => (index === 0 ? 50 : 20)}
           rowCount={gridStore.deviceIds.size + 1}
           style={STYLE}
           styleBottomLeftGrid={STYLE_BOTTOM_LEFT_GRID}
