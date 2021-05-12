dd9d7fbfe11e1c566dc5376fe6242699e76e44eb
daniel <dpletzke@gmail.com>
2021-05-12 00:20:58 -0500

    limit and order total logs displayed

--- client/src/views/Logs.jsx
+++ client/src/views/Logs.jsx
@@ -21,7 +21,6 @@ const Logs = observer(() => {
   const logRow = (msg) => {
     const obr = msg.segments.find((sg) => sg.name === "OBR");
     const obxs = msg.segments.filter((sg) => sg.name === "OBX");
-    console.log({ msg, obr, obxs });
     return (
       <tr key={msg.id}>
         <td>{msg.header.fields.sentTimeStamp}</td>
@@ -30,12 +29,15 @@ const Logs = observer(() => {
         <td>
           {obxs.map(({ fields }) => {
             const { observationIdentifier, value } = fields;
-            return <p>{`${observationIdentifier}, ${value}`}</p>;
-          })}
+            return `${observationIdentifier}, ${value}`;
+          }).join('| ')}
         </td>
       </tr>
     );
   };
+
+  const totalLogs = logsStore.logs.length;
+  const displayLogs = logsStore.logs.slice(totalLogs - 10).map((log) => logRow(log)).reverse();
 
   return (
     <Container fluid>
@@ -44,7 +46,7 @@ const Logs = observer(() => {
           <Card className="card-plain table-plain-bg">
             <Card.Header>
               <Card.Title as="h4">All Logs</Card.Title>
-              <p className="card-category">Last 200 messages</p>
+              <p className="card-category">Last 10 messages</p>
             </Card.Header>
             <Card.Body className="table-full-width table-responsive px-0">
               <Table className="table-hover">
@@ -56,7 +58,7 @@ const Logs = observer(() => {
                     <th className="border-0">Variables</th>
                   </tr>
                 </thead>
-                <tbody>{logsStore.logs.map((log) => logRow(log))}</tbody>
+                <tbody>{displayLogs}</tbody>
               </Table>
             </Card.Body>
           </Card>
