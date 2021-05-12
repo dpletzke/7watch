fd6e1bc64e3f25033b881ecc11b61b13840e7319
daniel <dpletzke@gmail.com>
2021-05-12 00:12:07 -0500

    add color changing background based on time since last received

--- client/src/components/Grid/Square.jsx
+++ client/src/components/Grid/Square.jsx
@@ -1,11 +1,14 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { observer } from "mobx-react-lite";
 
 import { useGridStore } from "../../contexts";
+import { autorun } from "mobx";
 
 const Square = observer((props) => {
   const { columnIndex, rowIndex } = props;
   const gridStore = useGridStore();
+
+  const [color, setColor] = useState('white');
 
   const [deviceId, observationId] = useMemo(() => {
     // offset row and column index for header columns
@@ -13,10 +16,26 @@ const Square = observer((props) => {
     return ids;
   }, [columnIndex, gridStore, rowIndex]);
 
+  useEffect(() => {
+    let timer;
+    autorun(() => {
+      gridStore.getValue(deviceId, observationId);
+      setColor('yellow');
+      timer = setTimeout(() => {
+        setColor('white');
+      }, 2300);
+    })
+    return () => {
+      clearInterval(timer);
+    }
+  }, []);
+  
   const title = `${deviceId} - ${observationId}`;
   const value = gridStore.getValue(deviceId, observationId);
+  
 
-  return <div title={title}>{value}</div>;
+
+  return <div title={title} style={{backgroundColor: color}}>{value}</div>;
 });
 
 export default Square;
